b7c624c6ff852c31c617e6766c1a5cf331fb9d11
molecule-lab-rushil <rushildhinoja@moleculelab.in>
2025-09-13 21:46:07 +0530

    feat(otter-server): configure cors for public routes

--- apps/otter-server/src/plugins/external/cors.ts
+++ apps/otter-server/src/plugins/external/cors.ts
@@ -1,17 +1,44 @@
-/**
- * This plugins enables the use of CORS.
- *
- * @see {@link https://github.com/fastify/fastify-cors}
- */
+// /**
+//  * This plugins enables the use of CORS.
+//  *
+//  * @see {@link https://github.com/fastify/fastify-cors}
+//  */
 
 import cors, { FastifyCorsOptions } from "@fastify/cors"
+import { FastifyInstance, FastifyRequest } from "fastify"
+import fp from "fastify-plugin"
 
-export const autoConfig: FastifyCorsOptions = {
-  origin: "http://localhost:3001",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  credentials: true,
-  maxAge: 86400,
+async function rateLimitPlugin(fastify: FastifyInstance) {
+  await fastify.register(cors, () => {
+    return (
+      request: FastifyRequest,
+      callback: (error: Error | null, corsOptions: FastifyCorsOptions) => void,
+    ) => {
+      const corsOptions = {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: [
+          "Content-Type",
+          "Authorization",
+          "X-Requested-With",
+          "x-api-key",
+        ],
+        credentials: true,
+        maxAge: 86400,
+        preflightContinue: true,
+      }
+
+      //Todo: Allow Set Origins for public URL
+      if (request.raw.url === "/" || request.raw.url?.includes("public")) {
+        corsOptions.origin = "*"
+      }
+
+      // callback expects two parameters: error and options
+      callback(null, corsOptions)
+    }
+  })
 }
 
-export default cors
+export default fp(rateLimitPlugin, {
+  name: "cors",
+})
